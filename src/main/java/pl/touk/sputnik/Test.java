b4b6516db789b2b7d2f5de0d62148eacca25904c
Piotr Jagielski <pjg@touk.pl>
2016-04-08 21:54:19 +0200

    initial

---
+++ src/main/java/pl/touk/sputnik/Test.java
@@ -0,0 +1,14 @@
+package pl.touk.sputnik;
+
+public class Test {
+
+    public boolean test = false;
+
+    public int method() {
+        if (test) {
+            return 1;
+        }
+        return 2;
+    }
+
+}
