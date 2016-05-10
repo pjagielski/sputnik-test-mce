878660e97170cb3503d289152a9a5bc556b564bb
Piotr Jagielski <pjg@touk.pl>
2016-05-10 13:35:55 +0200

    hello wjug

--- src/main/java/pl/touk/sputnik/Test.java
+++ src/main/java/pl/touk/sputnik/Test.java
@@ -5,10 +5,11 @@ public class Test {
     public boolean test = false;
 
     public int method() {
+        String str = null;
         if (test) {
             return 1;
         }
-        return 2;
+        return str.length();
     }
 
 }
